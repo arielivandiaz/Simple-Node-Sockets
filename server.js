226e85daa0b2a620303e91763c3947703bef1f97
Ariel Ivan Diaz <arielivandiaz@gamil.com>
2018-11-09 13:56:47 -0300

    updated

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-
+var morgan = require('morgan'); //Request logger
 var express = require('express'); //Express main framework
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 var port = process.env.PORT || 3838;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev')); // Set morgan to log every request to the console
+
+app.set('view engine', 'ejs'); // Set up ejs how templates motor
+
 
 require('./app/routes.js')(app,io); 
 
