b7c606de60f46f05d2af617e55f0be81fa490d86
Ariel Ivan Diaz <arielivandiaz@gamil.com>
2018-11-09 13:34:27 -0300

    Version 1

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var express = require('express'); //Express main framework
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
-const PORT = 3838;
+var port = process.env.PORT || 3838;
 
 require('./app/routes.js')(app,io); 
 
-server.listen(PORT);
+server.listen(port);
 console.log('Server is running');
 
 
